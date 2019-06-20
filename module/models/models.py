b766463299b5226983de66323dffbae8ab6e76d0
@r.pikh <r.pikh@seclgroup.com>
2019-06-20 13:43:18 +0300

    ad12

--- module/models/models.py
+++ module/models/models.py
@@ -2,15 +2,22 @@
 
 from odoo import models, fields, api
 
+#
+# class module(models.Model):
+#     _name = 'module.module'
+#
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
 
-class module(models.Model):
-    _name = 'module.module'
+class Partner(models.Model):
+    _inherit = 'res.partner'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
+    test_field = fields.Text('My New field')
+    test_field1 = fields.Text('My New field1')
+    test_field2 = fields.Text('My New field2')
