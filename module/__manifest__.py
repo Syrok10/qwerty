d200d362cab6150ea977d759937c8165f3232484
@r.pikh <r.pikh@seclgroup.com>
2019-06-21 13:55:39 +0300

    add new model

--- module/__manifest__.py
+++ module/__manifest__.py
@@ -20,13 +20,18 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
+
 
     # always loaded
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
+        'templates.xml',
         'views/views.xml',
-        'views/templates.xml',
+        'views/partner.xml',
+        'views/session_board.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
