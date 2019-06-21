4aaa58471399672d35aa6289ff6c1816123f4916
@r.pikh <r.pikh@seclgroup.com>
2019-06-21 14:23:46 +0300

    add new openacademy

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -20,13 +20,17 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'templates.xml',
+        'views/openacademy.xml',
+        'views/partner.xml',
+        'views/session_board.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
