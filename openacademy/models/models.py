92fd54f8e7b81c15b54b4232a4d655ec08b67409
@r.pikh <r.pikh@seclgroup.com>
2019-06-24 16:21:07 +0300

    add1

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -92,10 +92,11 @@ class Session(models.Model):
             if not (r.start_date and r.duration):
                 r.end_date = r.start_date
                 continue
+
             # Add duration to start_date, but: Monday + 5 days = Saturday, so
             # subtract one second to get on Friday instead
             duration = timedelta(days=r.duration, seconds=-1)
-            r.end_date = str(r.start_date + duration)
+            r.end_date = r.start_date + duration.strftime('%d/%m/%Y')
 
     def _set_end_date(self):
         for r in self:
