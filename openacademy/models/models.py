e60d729007e2066dc4e617fa56a1613f5c08202a
@r.pikh <r.pikh@seclgroup.com>
2019-06-24 12:46:58 +0300

    add1

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -9,3 +9,13 @@ class openacademy(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = "Openacademy Sessions"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
