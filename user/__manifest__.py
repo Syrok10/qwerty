55d4e998fec517fea6e7b8d4db802c4e8b6cb95e
@r.pikh <r.pikh@seclgroup.com>
2019-07-04 10:07:08 +0300

    user

--- user/__manifest__.py
+++ user/__manifest__.py
@@ -1,38 +1,24 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
-    'name': "user",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+    'name': 'Signup',
     'description': """
-        Long description of module's purpose
+Allow users to sign up and reset their password
+===============================================
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'auto_install': True,
     'depends': [
         'base_setup',
         'mail',
-        'web'
+        'web',
     ],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+
         'views/auth_signup_login_templates.xml',
+
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'bootstrap': True,
 }
